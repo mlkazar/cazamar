ffd97219034e4662e5d3e52e67368c7bda7580f3
Michael Kazar <michael.kazar@verizon.net>
2020-06-11 17:56:03 -0400

    miscellaneous minor changes

---
+++ mfand/config.js
@@ -0,0 +1,10 @@
+{
+"backupInt" : 86400,"backupEntries" :
+[
+{
+"cloudRoot" : "/kazar_backup/Pictures","fsRoot" : "/Users/kazar/Pictures","lastFinishedTime" : 1568283717,"enabled" : 0},
+{
+"cloudRoot" : "/kazar_backup/uptest","fsRoot" : "/Users/kazar/uptest","lastFinishedTime" : 1589377728,"enabled" : 1}
+]
+}
+
